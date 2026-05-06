d28a475acb0bc40b0160c9c5768beedf8c123d72
kirshe <kirshe@yandex-team.com>
2026-05-06 09:54:53 +0500

    upd images

--- deploy/terraform/back.tf
+++ deploy/terraform/back.tf
@@ -28,7 +28,7 @@ resource "yandex_serverless_container" "tts" {
   }
 
   image {
-      url = "cr.yandex/sol/speechkit-workbench/tts-service:1.1.0"
+      url = "cr.yandex/sol/speechkit-workbench/tts-service:1.1.1"
       environment = {
           S3_BUCKET = yandex_storage_bucket.front.bucket
       }
@@ -64,7 +64,7 @@ resource "yandex_serverless_container" "stt" {
   }
 
   image {
-      url = "cr.yandex/sol/speechkit-workbench/stt-service:1.1.0"
+      url = "cr.yandex/sol/speechkit-workbench/stt-service:1.1.1"
       environment = {
           S3_BUCKET = yandex_storage_bucket.front.bucket
           MODEL_URI = "gpt://${var.folder_id}/yandexgpt-5.1"
